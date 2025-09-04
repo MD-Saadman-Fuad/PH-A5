ef2724fda4ff993960af6a4e8d4d93aac920986d
MD-Saadman-Fuad <md.saadman.fuad@gmail.com>
2025-09-05 00:04:16 +0600

    html and js done

---
+++ script.js
@@ -0,0 +1,51 @@
+console.log('connected')
+// cards heart button click will increase count by 1 Done
+const heartButtons = document.getElementsByClassName('heart-btn');
+for (let i = 0; i < heartButtons.length; i++) {
+    heartButtons[i].addEventListener('click', function() {
+        const totalHeartElement = document.getElementById('total-heart');
+        totalHeartElement.innerText = parseInt(totalHeartElement.innerText) + 1;
+    });
+}
+
+// Call Button Functionality Done
+
+const callButtons = document.getElementsByClassName('call-btn');
+const callNumbers = document.getElementsByClassName('call-number');
+const serviceNames = document.getElementsByClassName('service-name');
+
+
+let availableCoins = parseInt(document.getElementById('available-coin').innerText);
+for (let i = 0; i < callButtons.length; i++) {
+    callButtons[i].addEventListener('click', function() {
+        const callHistory = document.getElementById('call-history');
+        const phoneNumber = callNumbers[i].innerText;
+        if(availableCoins < 20) {
+            alert("Insufficient coins to make a call.");
+            return;
+        }
+        alert(`${serviceNames[i].innerText}\nCalling ${phoneNumber}`);
+        availableCoins -= 20;
+        document.getElementById('available-coin').innerText = availableCoins;
+
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <div class="history-card flex items-center justify-between p-4 bg-gray-100 rounded-lg mb-4">
+                <div>
+                    <h3 class="font-bold text-lg" id="service-name-called">${serviceNames[i].innerText}</h3>
+                    <p class="text-gray-500 service-name" id="service-number-called">${phoneNumber}</p>
+
+                </div>
+                <div class="items-top text-right">
+                    <p class="text-gray-500 call-time" id="call-time-called">${new Date().toLocaleTimeString()}</p>
+                </div>
+            </div>  
+        `;
+        // Insert new call at the top of the sidebar
+        if (callHistory.firstChild) {
+            callHistory.insertBefore(div, callHistory.firstChild);
+        } else {
+            callHistory.appendChild(div);
+        }
+    });
+}
